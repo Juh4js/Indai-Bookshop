82ec12ee35c74e22c30428e3044855cacf17a0e1
Henrique Hack <henriquehack2008@gmail.com>
2024-08-09 18:41:19 -0300

    indaia_bookshop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { ProdutoDetalhesComponent } from './produto-detalhes/produto-detalhes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent,
+    ProdutoDetalhesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/produto-detalhes/produto-detalhes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-produto-detalhes',
+  templateUrl: './produto-detalhes.component.html',
+  styleUrls: ['./produto-detalhes.component.scss']
+})
+export class ProdutoDetalhesComponent implements OnInit {
+  produtoId: string | null = null;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.produtoId = this.route.snapshot.paramMap.get('id');
+    // Aqui você deve buscar os detalhes do produto com base no produtoId
+    // Por exemplo, usando um serviço que faz uma chamada HTTP
+  }
+}
