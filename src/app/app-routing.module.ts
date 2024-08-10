fd56d22a2d6c145b9bbf5135648914e41215f36b
Henrique Hack <henriquehack2008@gmail.com>
2024-08-09 21:51:59 -0300

    indaia_bookshop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { LoginComponent } from './pages/login/login.component';  // Importe o LoginComponent
 
 const routes: Routes = [
   { path: 'catalogo', component: CatalogoComponent },
+  { path: 'login', component: LoginComponent },  // Adicione esta linha para a rota de login
   { path: '', redirectTo: 'catalogo', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
-import { ProdutoDetalhesComponent } from './produto-detalhes/produto-detalhes.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogoComponent,
-    ProdutoDetalhesComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -19,3 +19,4 @@ import { ProdutoDetalhesComponent } from './produto-detalhes/produto-detalhes.co
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
